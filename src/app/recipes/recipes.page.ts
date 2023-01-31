545153073cc9dfd4e1834e1d4ff39e77a75c5a70
lhhernandez-isc <hhernandez.isc@gmail.com>
2023-01-31 10:07:03 -0600

    1: Page caching & Extra LifeCycle Hooks

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -14,6 +14,24 @@ export class RecipesPage implements OnInit {
   constructor(private recipesService: RecipesService) {}
 
   ngOnInit() {
+    // this.recipes = this.recipesService.getAllRecipes();
+    console.log(this.recipes);
+  }
+
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter');
     this.recipes = this.recipesService.getAllRecipes();
+  }
+
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter');
+  }
+
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave');
+  }
+
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave');
   }
 }
